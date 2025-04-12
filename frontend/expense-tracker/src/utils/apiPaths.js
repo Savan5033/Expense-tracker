193d9573fb7a7ff950a63229942e149106bbfdae
Savan5033 <savanpatel14478@gmail.com>
2025-04-13 00:46:13 +0530

    backend deploye

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:5000/";
+export const BASE_URL = "https://vercel.com/savan5033s-projects/expense-tracker-p1nd-api/";
 
 export const API_PATHS = {
   AUTH: {
